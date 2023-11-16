0bc528044066ad3659f4ba743a89ff4a38ce7e91
JerlynTagopa <146070025+JerlynTagopa@users.noreply.github.com>
2023-11-16 23:45:07 +0800

    Add files via upload

---
+++ Oop Main.java
@@ -0,0 +1,13 @@
+
+	package oop_project;
+
+public class OOP_main {
+   
+    public static void main(String[] args) {
+    
+    Food food_01 = new Food("Siomai Rice",35.00);
+    
+    food_01.showFoodName();
+    food_01.showFoodPrice();
+}
+}
